ed7775ccbf51be11a1706568194d8b1a27a8905d
Johann Williams <johann.williams01@gmail.com>
2023-07-21 12:40:38 -0500

    completed sessions and cookies first exercise. locking down profile page

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,11 +3,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+
+        if(session.getAttribute("user") != null){
+            request.getRequestDispatcher("/profile").forward(request, response);
+        }
+
         request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
     }
 
@@ -17,6 +25,8 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
