406b11f67af35957be4807c23595a76bba270cc3
Johann Williams <johann.williams01@gmail.com>
2023-07-24 09:36:49 -0500

    updated gitignore and changed logout to use session invalidate method not just remove session attribute.

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -11,7 +11,7 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        session.removeAttribute("user");
+        session.invalidate();
         req.getRequestDispatcher("/login").forward(req, resp);
     }
 }
